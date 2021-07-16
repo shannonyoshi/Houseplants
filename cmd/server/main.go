70c0e0fe7ee8e0aa32175705a2ba7a86de5efc04
Shannon Yoshikawa <shannon.yoshikawa@gmail.com>
2021-07-15 20:59:39 -0400

    set up and tested server with static files

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+
+	"github.com/gorilla/handlers"
+)
 
 func main() {
+	mux := http.NewServeMux()
+
+	newHandler := func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("hello world!"))
+	}
+
+	mux.HandleFunc("/hello", newHandler)
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/", fs)
+
+	wrap := handlers.LoggingHandler(os.Stdout, mux)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		err := http.ListenAndServe(":3030", wrap)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		wg.Done()
+	}()
+
 	fmt.Println("vim-go")
+	wg.Wait()
 }
